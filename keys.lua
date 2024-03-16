6382434331c843ff0df3873b0657296f930fbd45
03jdops <114336560+03jdops@users.noreply.github.com>
2024-03-16 11:15:03 +0000

    Create keys.lua

---
+++ keys.lua
@@ -0,0 +1,35 @@
+{
+    "s15x USERID" : {
+      "USERID" :  500595751
+    },
+    
+    "WtQp3jq5Ex": {
+        "SystemLicense": true,
+        "ChargeBack" : false,
+        "KickMsg": "Nil",
+        "System Owner": {
+            "DiscordID": 881474374413209610,
+            "RobloxID": 2371800278
+        }
+    },
+    
+    "45A6nXFUmM": {
+        "SystemLicense": false,
+        "ChargeBack" : false,
+        "KickMsg": "API Key hasnt been activated or is still being acivated",
+        "System Owner": {
+            "DiscordID": 0,
+            "RobloxID": 0
+        }
+    },
+    
+    "6PpBHz7B9V": {
+        "SystemLicense": false,
+        "ChargeBack" : false,
+        "KickMsg": "API Key hasnt been activated or is still being acivated",
+        "System Owner": {
+            "DiscordID": 0,
+            "RobloxID": 0
+        }
+    }
+}
