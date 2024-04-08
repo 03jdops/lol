0e3a496dfcc0f5421025fea54c6c5b008029f1fc
03jdops <114336560+03jdops@users.noreply.github.com>
2024-04-08 18:39:19 +0100

    Update keys.lua

--- keys.lua
+++ keys.lua
@@ -130,12 +130,12 @@
         }
     },
             "3Z8HKxiMPudupgnF2r0z": {
-        "SystemLicense": false,
+        "SystemLicense": true,
         "ChargeBack" : false,
-        "KickMsg": "API Key hasnt been activated or is still being acivated please wait 5 minutes",
+        "KickMsg": "API Key has been activated | V4 System",
         "System Owner": {
-            "DiscordID": 0,
-            "RobloxID": 0
+            "DiscordID": 707214699024089178,
+            "RobloxID": 2980577308
         }
     },
             "hE4A42RyoNqQKAZ3Gf4h": {
